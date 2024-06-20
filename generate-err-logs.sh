bea39b5158cd6dac6f1bd032e72ffc0a6ca73ee8
shakedaviv50313 <saviv@redhat.com>
2024-06-20 11:58:27 +0300

    chore(RHTAPWATCH-1043): fix kubelinter root-related issues
    fix Kubelinter security related issues in Konflux ci
    
    Signed-off-by: Shaked Aviv <saviv@redhat.com>

--- generate-err-logs.sh
+++ generate-err-logs.sh
@@ -19,7 +19,10 @@ generate_logs() {
     local namespaces
     namespaces=$(kubectl get namespaces -o name | xargs -n1 basename)
 
+    echo "$namespaces"
+
     for namespace in $namespaces; do
+        echo "$namespace"
         # Get all 'Warning' type events that occured on the namespace and extract the relevant fields from it as variables.
         local events
         events=$(kubectl get events -n "$namespace" \
@@ -27,17 +30,22 @@ generate_logs() {
                 -o jsonpath='{range .items[*]}{.involvedObject.kind}{" "}{.involvedObject.name}{" "}{.message}{" ("}{.reason}{")\n"}{end}'
         
         )
+        echo "$events"
 
         echo "$events" | while read -r kind name message reason; do
+            echo "Processing kind=$kind name=$name message=$message reason=$reason"
             if [ "$kind" == "Pod" ]; then
                 local pod_definition
                 local pod_logs
+                echo "pod_definition anchor"
                 pod_definition=$(kubectl get pod -n "$namespace" "$name" -o yaml 2>&1)
-                pod_logs=$(kubectl logs -n "$namespace" "$name" --all-containers=true 2>&1)
-
+                echo "pod_logs anchor"
+                pod_logs=$(kubectl logs -n "$namespace" "$name" --all-containers=true 2>&1 || echo "Failed to get pod logs for $name in namespace $namespace")                echo "after pod_logs"
                 printf "%s\n---\n" "$pod_definition" | tee -a "$pod_definitions_file"
                 echo "Pod '$name' under namespace '$namespace':" | tee -a "$pod_logs_file" | tee -a "$event_messages_file"
+                echo "pod events:"
                 echo "$kind $name $message $reason" | tee -a "$event_messages_file"
+                echo "pod logs:"
                 echo "$pod_logs" | tee -a "$pod_logs_file"
         fi
     done
@@ -47,5 +55,3 @@ done
 if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then
     main "$@"
 fi
-
-
